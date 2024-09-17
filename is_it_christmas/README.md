d70d57d35b2d15ff59bd0570405c4a1ebb7676c2
nayakiselchik <nayakiselchik@gmail.com>
2024-09-17 19:36:39 +0300

    the whole thing basically

---
+++ is_it_christmas/README.md
@@ -0,0 +1,27 @@
+# "Чи вже Різдво" та Секретний Санта
+
+## Завантаження:
+
+1. Створити та активувати віртуальне середовище:
+Для Linux та macOS:
+```bash
+python -m venv venv source venv/bin/activate
+```
+Windows:
+```bash
+venv\Scripts\activate
+```
+
+2. Встановіть залежності:
+```bash
+pip install -r requirements.txt
+```
+3. Запустіть міграції:
+```bash
+python manage.py migrate
+```
+4. Запустіть сервер:
+```bash
+python manage.py runserver
+```
+5. Зайдіть на `http://127.0.0.1:8000/`, щоб почати користуватися програмою.
--- is_it_christmas/secret_santa/views.py
+++ is_it_christmas/secret_santa/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from datetime import date
+import random
+from .models import Participant
+from .forms import ParticipantForm
 
-# Create your views here.
+
+def is_it_christmas_today(request):
+    today = date.today()
+    is_christmas = today.month == 12 and today.day == 25
+    return render(request, "christmas.html", {"is_it_christmas": is_christmas})
+
+
+def game(request):
+    participants = Participant.objects.all()
+    if request.method == "POST":
+        form = ParticipantForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("game")
+    else:
+        form = ParticipantForm()
+
+    if "generate_pairs" in request.POST:
+        pairs = generate_santa_pairs(participants)
+        return render(request, "pairs.html", {"pairs": pairs})
+
+    return render(
+        request,
+        "secret_santa.html",
+        {"form": form, "participants": participants},
+    )
+
+
+def generate_santa_pairs(participants):
+    givers = list(participants)
+    while True:
+        receivers = list(participants)
+        random.shuffle(receivers)
+        if all(giver != receiver for giver, receiver in zip(givers, receivers)):
+            break
+    pairs = list(zip(givers, receivers))
+    return pairs
+
+
+def delete_all_participants(request):
+    if request.method == "POST":
+        Participant.objects.all().delete()
+        return redirect("game")
+    return redirect("game")
